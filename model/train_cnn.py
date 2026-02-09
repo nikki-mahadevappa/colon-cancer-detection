ef2803c0429dc4ea2f3f9f18642c5b00b7652bcf
Niharika Mahadevappa <niharikamahadevappa0@gmail.com>
2026-02-09 18:03:30 +0530

    Add files via upload

--- model/train_cnn.py
+++ model/train_cnn.py
@@ -1,1 +1,58 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+DATASET_PATH = "data/images"
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 16
+EPOCHS = 5
+
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train_data = datagen.flow_from_directory(
+    DATASET_PATH,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='binary',
+    subset='training'
+)
+
+val_data = datagen.flow_from_directory(
+    DATASET_PATH,
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode='binary',
+    subset='validation'
+)
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(224,224,3)),
+    MaxPooling2D(2,2),
+
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=EPOCHS
+)
+
+model.save("model/colon_cancer_cnn.h5")
+
+print("Model training complete and saved.")
